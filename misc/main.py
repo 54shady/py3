d139654733ac559757db9d48abd8ea8436b1a84b
zeroway <M_O_Bz@163.com>
2016-10-18 19:19:08 +0000

    python parameter without order

--- misc/main.py
+++ misc/main.py
@@ -2,6 +2,7 @@
 
 from find_max import return_max_number
 from append_items import append_all
+from parameter_without_order import describe_feature
 
 if __name__ == "__main__":
     print(return_max_number(1))
@@ -17,5 +18,8 @@ if __name__ == "__main__":
     print(names)
     append_all(names, "zeroway", "mobz", "shady", "eminem")
     print(names)
+
+    print(describe_feature(name = 'zeroway', age = 28, weight = 120, species = 'mobz'))
+    print(describe_feature(age = 18, weight = 100, species = 'eminem', name = 'shady'))
 
 # vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
---
+++ misc/parameter_without_order.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+def describe_feature(name, species, age, weight):
+    return '%s(%s) %d years %d kg' % (name, species, age, weight)
+
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
