8c7a1ce900b61a4101410221f6c22e21038684e8
zeroway <M_O_Bz@163.com>
2016-10-19 14:54:31 +0000

    open subprocess

---
+++ chapter15/open_subprocess.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+import subprocess
+subprocess.Popen('/usr/bin/kcalc')
+subprocess.Popen(['/usr/bin/kwrite', '/home/zeroway/github/py3/README.md'])
+
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
